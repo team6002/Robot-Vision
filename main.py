850be3908517eb3b446e5c98427963c089886318
mishiki1002 <Brownbear1002@gmail.com>
2017-05-19 19:46:28 -0400

    Added Window with titled window

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import cv2
 #Loading image in gray scale
 img = cv2.imread('image.jpg',0)
 
-cv2.imshow('image',img)
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
