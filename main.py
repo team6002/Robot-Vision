94bdedcfcdd9a1f12ea4919ec4a77ed3f0f4c5ae
mishiki1002 <Brownbear1002@gmail.com>
2017-05-19 19:57:55 -0400

    Converter

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import numpy as np
 import cv2
 
-#Loading image in gray scale
 img = cv2.imread('image.jpg',0)
-
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('messigray.png',img)
+    cv2.destroyAllWindows()
