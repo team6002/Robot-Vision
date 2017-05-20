f83e098531d055f7c0840bfb0819b17ed79991c7
mishiki1002 <Brownbear1002@gmail.com>
2017-05-19 20:23:20 -0400

    Color picker boi

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-import numpy as np
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+def nothing(x):
+    pass
 
-while(True):
-    #Capture frame-by-frame
-    ret, frame = cap.read()
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
+cv2.namedWindow('image')
 
-    #Our operation on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# create trackbars for color change
+cv2.createTrackbar('R','image',0,255,nothing)
+cv2.createTrackbar('G','image',0,255,nothing)
+cv2.createTrackbar('B','image',0,255,nothing)
 
-    #Display the resulting frame
-    cv2.imshow('Screen',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+# create switch for ON/OFF functionality
+switch = '0 : OFF \n1 : ON'
+cv2.createTrackbar(switch, 'image',0,1,nothing)
+
+while(1):
+    cv2.imshow('image',img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
         break
 
-cap.rlease()
+    # get current positions of four trackbars
+    r = cv2.getTrackbarPos('R','image')
+    g = cv2.getTrackbarPos('G','image')
+    b = cv2.getTrackbarPos('B','image')
+    s = cv2.getTrackbarPos(switch,'image')
+
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b,g,r]
+
 cv2.destroyAllWindows()
 
