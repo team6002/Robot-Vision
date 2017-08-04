73b04cce8fc23d6917d3bd099147a23981f64db3
Your Name <you@example.com>
2017-08-04 01:04:30 +0000

    Tested Stuff

--- testcase/record.py
+++ testcase/record.py
@@ -1,8 +1,9 @@
-iport numpy as np
+import numpy as np
 import time
 import cv2
 
 print("Writing Video")
+cap = cv2.VideoCapture(0)
 out = cv2.VideoWriter('output.avi', 0, 20.0, (640,480))
 
 time.sleep(20)
