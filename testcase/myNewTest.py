b634e5371b40fbc9a6e5937faf847ce8bca5f4d6
Your Name <you@example.com>
2017-08-04 01:17:39 +0000

    Added new Test #5

---
+++ testcase/myNewTest.py
@@ -0,0 +1,28 @@
+import numpy as np
+import time
+import cv2
+
+cap = cv2.VideoCapture(0)
+size = (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+
+fps = 20
+fourcc = 0 
+vout = cv2.VideoWriter()
+success = vout.open('new.avi',fourcc,fps,size,True) 
+ret, frame = cap.read()
+
+frame = cv2.flip(frame, -1)
+
+print("Writing")
+counter = 0
+while(counter < 10):
+	vout.write(frame) 
+	time.sleep(1)
+	counter = counter + 1
+print("exiting")
+
+cap.release()
+vout.release()
+cv2.destroyAllWindows()
+
