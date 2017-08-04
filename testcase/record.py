e7ae8ef040aaaa734e91ccba8dcff470f0ff378f
Your Name <you@example.com>
2017-08-04 01:20:56 +0000

    record now works testcase #6 sucsessfull

--- testcase/record.py
+++ testcase/record.py
@@ -2,11 +2,27 @@ import numpy as np
 import time
 import cv2
 
-print("Writing Video")
 cap = cv2.VideoCapture(0)
-out = cv2.VideoWriter('output.avi', 0, 20.0, (640,480))
+size = (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
 
-time.sleep(20)
+fps = 20
+fourcc = 0 
+vout = cv2.VideoWriter()
+success = vout.open('output.avi',fourcc,fps,size,True) 
+ret, frame = cap.read()
 
-print("Releasing Video")
-out.release()
+frame = cv2.flip(frame, -1)
+
+print("Writing")
+counter = 0
+while(counter < 50):
+	vout.write(frame) 
+	time.sleep(1)
+	counter = counter + 1
+print("exiting")
+
+cap.release()
+vout.release()
+cv2.destroyAllWindows()
+
